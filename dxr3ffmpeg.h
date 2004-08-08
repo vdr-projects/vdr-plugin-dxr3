1719792e23bbb44a8f3c815885fa339dac9c9401
austriancoder <austriancoder>
2004-08-08 22:16:57 +0000

    updated copyright informations

--- dxr3ffmpeg.h
+++ dxr3ffmpeg.h
@@ -1,3 +1,24 @@
+/*
+ * dxr3ffmpeg.h
+ *
+ * Copyright (C) 2004 Christian Gmeiner
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
 #ifndef _DXR3_FFMPEG_H_
 #define _DXR3_FFMPEG_H_
 
@@ -6,8 +27,6 @@ extern "C"
 	#include <avcodec.h>
 };
 
-//#include <stdlib.h>
-//#include <stdint.h>
 #include <string.h> 
 #include "dxr3singleton.h"
 
--- dxr3memcpy.c
+++ dxr3memcpy.c
@@ -1,4 +1,25 @@
 /*
+ * dxr3memcpy.c
+ *
+ * Copyright (C) 2004 Christian Gmeiner
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * Orgianal from xine:
+ *
  * Copyright (C) 2001-2003 the xine project
  *
  * This file is part of xine, a free video player.
