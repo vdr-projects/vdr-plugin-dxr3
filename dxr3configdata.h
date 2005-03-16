da2d2a1fa054d12df081a75d3db2d617c79d583e
scop <scop>
2005-03-16 09:02:29 +0000

    Sync some comments with HEAD.

--- dxr3configdata.h
+++ dxr3configdata.h
@@ -62,49 +62,49 @@ public:
     cDxr3ConfigData();
 	~cDxr3ConfigData()	{}
 
-    int GetUseDigitalOut() const						{ return m_Digitaloutput; }
-    int SetUseDigitalOut(int value)						{ return m_Digitaloutput = value; }
-    int GetDxr3Card() const								{ return m_Card; }
-    int SetDxr3Card(int value)							{ return m_Card = value; }
-    int GetForceLetterBox()	const						{ return m_ForceLetterbox; }
-    int SetForceLetterBox(int value)					{ return m_ForceLetterbox = value; }
-    int GetAc3OutPut() const							{ return m_AC3output; }
-    int SetAc3OutPut(int value)							{ return m_AC3output = value;}
+    int GetUseDigitalOut() const						{ return m_digitaloutput; }
+    int SetUseDigitalOut(int value)						{ return m_digitaloutput = value; }
+    int GetDxr3Card() const								{ return m_card; }
+    int SetDxr3Card(int value)							{ return m_card = value; }
+    int GetForceLetterBox()	const						{ return m_forceletterbox; }
+    int SetForceLetterBox(int value)					{ return m_forceletterbox = value; }
+    int GetAc3OutPut() const							{ return m_ac3output; }
+    int SetAc3OutPut(int value)							{ return m_ac3output = value;}
 
-    eVideoMode GetVideoMode() const						{ return m_VideoMode;}
-    eVideoMode SetVideoMode(eVideoMode videoMode)		{ return m_VideoMode = videoMode;}
-    eMenuMode GetMenuMode()	const						{ return m_MenuMode; }
-    eMenuMode SetMenuMode(eMenuMode menuMode)			{ return m_MenuMode = menuMode; }
+    eVideoMode GetVideoMode() const						{ return m_videomode;}
+    eVideoMode SetVideoMode(eVideoMode videoMode)		{ return m_videomode = videoMode;}
+    eMenuMode GetMenuMode()	const						{ return m_menumode; }
+    eMenuMode SetMenuMode(eMenuMode menuMode)			{ return m_menumode = menuMode; }
 
-	int GetDebug() const								{ return m_Debug; }
-	int SetDebug(int value)								{ return m_Debug = value; }
-	int GetDebugLevel() const							{ return m_DebugLevel; }
-	int SetDebugLevel(int value)						{ return m_DebugLevel = value; }
+	int GetDebug() const								{ return m_debug; }
+	int SetDebug(int value)								{ return m_debug = value; }
+	int GetDebugLevel() const							{ return m_debuglevel; }
+	int SetDebugLevel(int value)						{ return m_debuglevel = value; }
 
 	// some little helpers to save some writing
-	int GetDebugLow() const								{ return (m_Debug && !m_DebugLevel); }
-	int GetDebugEverything() const						{ if (m_Debug && !m_DebugLevel) { return 1; } else { return 0; } }
+	int GetDebugLow() const								{ return (m_debug && !m_debuglevel); }
+	int GetDebugEverything() const						{ if (m_debug && !m_debuglevel) { return 1; } else { return 0; } }
 
-	int GetBrightness() const							{ return m_Brightness; }
-	int SetBrightness(int value)						{ return m_Brightness = value; }
-	int GetContrast() const								{ return m_Contrast; }
-	int SetContrast(int value)							{ return m_Contrast = value; }
-	int GetSaturation() const							{ return m_Saturation; }
-	int SetSaturation(int value)						{ return m_Saturation = value; }
+	int GetBrightness() const							{ return m_brightness; }
+	int SetBrightness(int value)						{ return m_brightness = value; }
+	int GetContrast() const								{ return m_contrast; }
+	int SetContrast(int value)							{ return m_contrast = value; }
+	int GetSaturation() const							{ return m_saturation; }
+	int SetSaturation(int value)						{ return m_saturation = value; }
 
 protected:
-    eVideoMode	m_VideoMode;///< videomode value
-    eMenuMode	m_MenuMode;	///< menumode value
+    eVideoMode	m_videomode;
+    eMenuMode	m_menumode;
 
-	int m_Digitaloutput;	///< digitaloutput value
-	int m_AC3output;		///< ac3output value
-	int m_Card;				///< card value
-	int m_ForceLetterbox;	///< forcebox value
-	int m_Debug;			///< debug value
-	int m_DebugLevel;		///< debug level value
-	int m_Brightness;		///< brightness value
-	int m_Contrast;			///< contrast value
-	int m_Saturation;		///< saturation value
+	int m_digitaloutput;
+	int m_ac3output;
+	int m_card;
+	int m_forceletterbox;
+	int m_debug;
+	int m_debuglevel;
+	int m_brightness;
+	int m_contrast;
+	int m_saturation;
 };
 
 #endif /*_DXR3_CONFIGDATA_H_*/
