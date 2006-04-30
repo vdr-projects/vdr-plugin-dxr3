71d083f7a30140fddbfeb59c536939e1b8d54970
scop <scop>
2006-04-30 14:16:33 +0000

    0.2.6.

--- dxr3.c
+++ dxr3.c
@@ -3,7 +3,7 @@
  *
  * See the README file for copyright information and how to reach the author.
  *
- * $Id: dxr3.c,v 1.1.2.25 2006/04/22 11:24:28 scop Exp $
+ * $Id: dxr3.c,v 1.1.2.26 2006/04/30 14:16:33 scop Exp $
  *
  */
 
@@ -15,7 +15,7 @@
 #include "dxr3.h"
 #include "dxr3i18n.h"
 
-static const char VERSION[]       = "0.2.6-cvs";
+static const char VERSION[]       = "0.2.6";
 static const char DESCRIPTION[]   = "Hardware MPEG decoder";
 static const char MAINMENUENTRY[] = "DXR3";
 
