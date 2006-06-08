ac0ead90609833bcb7b352b0e256b98c1be4ba50
scop <scop>
2006-06-08 15:10:01 +0000

    Bump to 0.2.7-cvs.

--- dxr3.c
+++ dxr3.c
@@ -3,7 +3,7 @@
  *
  * See the README file for copyright information and how to reach the author.
  *
- * $Id: dxr3.c,v 1.1.2.26 2006/04/30 14:16:33 scop Exp $
+ * $Id: dxr3.c,v 1.1.2.27 2006/06/08 15:10:01 scop Exp $
  *
  */
 
@@ -15,7 +15,7 @@
 #include "dxr3.h"
 #include "dxr3i18n.h"
 
-static const char VERSION[]       = "0.2.6";
+static const char VERSION[]       = "0.2.7-cvs";
 static const char DESCRIPTION[]   = "Hardware MPEG decoder";
 static const char MAINMENUENTRY[] = "DXR3";
 
