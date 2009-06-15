4b3a5c8e9249e1247ba721676323a34e9e79e6fa
Ville Skyttä <ville.skytta@iki.fi>
2009-06-15 21:46:22 +0300

    Update copyright boilerplates from http://www.gnu.org/licenses/lgpl-2.1.txt and http://www.gnu.org/licenses/gpl-2.0.txt

--- dxr3interface_spu_encoder.c
+++ dxr3interface_spu_encoder.c
@@ -21,10 +21,11 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
 
 #include "dxr3interface_spu_encoder.h"
 #include "dxr3tools.h"
--- dxr3interface_spu_encoder.h
+++ dxr3interface_spu_encoder.h
@@ -21,11 +21,11 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-*/
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
 
 #ifndef _DXR3_INTERFACE_SPU_ENCODER_
 #define _DXR3_INTERFACE_SPU_ENCODER_
