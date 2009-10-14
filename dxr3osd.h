79956361579c8474684b2bd6509f282eb81425c5
Christian Gmeiner <christian.gmeiner@gmail.com>
2009-10-14 19:37:11 +0200

    update/add license

--- dxr3osd.h
+++ dxr3osd.h
@@ -1,3 +1,29 @@
+/*
+ *           _                 _             _                 _          _____
+ * __   ____| |_ __      _ __ | |_   _  __ _(_)_ __         __| |_  ___ _|___ /
+ * \ \ / / _` | '__|____| '_ \| | | | |/ _` | | '_ \ _____ / _` \ \/ / '__||_ \
+ *  \ V / (_| | | |_____| |_) | | |_| | (_| | | | | |_____| (_| |>  <| |  ___) |
+ *   \_/ \__,_|_|       | .__/|_|\__,_|\__, |_|_| |_|      \__,_/_/\_\_| |____/
+ *                      |_|            |___/
+ *
+ * Copyright (C) 2009 Christian Gmeiner
+ *
+ * This file is part of vdr-plugin-dxr3.
+ *
+ * vdr-plugin-dxr3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2.
+ *
+ * vdr-plugin-dxr3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dxr3-plugin.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef _DXR3_OSD_H_
 #define _DXR3_OSD_H_
 
