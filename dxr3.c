8169f21314c4f2a3875252eecdf64d171e7a86f2
Ville Skyttä <ville.skytta@iki.fi>
2009-12-29 19:07:15 +0200

    Set version to 0.3.0-git.

--- dxr3.c
+++ dxr3.c
@@ -11,7 +11,7 @@
 #include "dxr3device.h"
 #include "dxr3.h"
 
-static const char VERSION[]       = "0.2.9+git";
+static const char VERSION[]       = "0.3.0-git";
 static const char DESCRIPTION[]   = trNOOP("Hardware MPEG decoder");
 
 #if APIVERSNUM && APIVERSNUM < 10600
